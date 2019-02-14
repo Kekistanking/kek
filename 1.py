770fa2661b3dea57b05cad8de7b0f8ac96ab5abd
Kekistanking <mishugan228@gmail.com>
2019-02-14 11:23:10 +0500

    ???

--- 1.py
+++ 1.py
@@ -5,11 +5,17 @@ import os
 FPS = 60
 
 pygame.init()
-size = width, height = 800, 600
+size = width, height = 960, 540
 screen = pygame.display.set_mode(size)
 const = 3
 mobs = pygame.sprite.Group()
+
 all_sprites = pygame.sprite.Group()
+mob_group = pygame.sprite.Group()
+player_group = pygame.sprite.Group()
+player_bullets = pygame.sprite.Group()
+vertical_borders = pygame.sprite.Group()
+horizontal_borders = pygame.sprite.Group()
 
 
 def load_image(name, colorkey=None):
@@ -23,53 +29,43 @@ def load_image(name, colorkey=None):
     image.convert_alpha()
     if colorkey is not None:
         if colorkey is -1:
-            colorkey = image.get_at((0, 0))
+            colorkey = image.get_at((3, 3))
+        image.set_colorkey(colorkey)
 
     return image
 
 
-class Board:
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
-        self.board = [[0] * width for _ in range(height)]
-        self.left = 10
-        self.top = 10
-        self.cell_size = 30
+room_image = load_image("room1.bmp")
+room_image = pygame.transform.scale(room_image, (960, 540))
 
-    def set_view(self, left, top, cell_size):
-        self.left = left
-        self.top = top
-        self.cell_size = cell_size
 
-    def render(self):
-
-        for i in range(self.height):
-            for q in range(self.width):
-                x = (self.cell_size, self.cell_size)
-                start = (self.left + self.cell_size * q, self.top + self.cell_size * i)
-                pygame.draw.rect(screen, pygame.Color(255, 255, 255), start + x, 1)
+class Border(pygame.sprite.Sprite):
+    # строго вертикальный или строго горизонтальный отрезок
+    def __init__(self, x1, y1, x2, y2):
+        super().__init__()
+        if x1 == x2:  # вертикальная стенка
+            self.add(vertical_borders)
+            self.image = pygame.Surface([3, y2 - y1])
+            self.rect = pygame.Rect(x1, y1, 3, y2 - y1)
+        else:  # горизонтальная стенка
+            self.add(horizontal_borders)
+            self.image = pygame.Surface([x2 - x1, 1])
+            self.rect = pygame.Rect(x1, y1, x2 - x1, 1)
 
 
 class Player(pygame.sprite.Sprite):
-    image = load_image("Pudge.png")
-    images_down = []
-    images_up = []
-    images_left = []
-    images_right = []
-    for i in range(8):
-        images_up.append(load_image("back_%d.png" % i))
-        images_down.append(load_image("back_%d.png" % i))
-        images_left.append(load_image("back_%d.png" % i))
-        images_right.append(load_image("back_%d.png" % i))
+    sheet = load_image("playersanim.bmp", -1)
 
     def __init__(self, cords):
-        super().__init__(all_sprites)
-        self.image = Player.image
+        super().__init__(player_group, all_sprites)
         self.x = cords[0]
         self.y = cords[1]
-        self.image = pygame.transform.scale(self.image, (50, 50))
-        self.rect = self.image.get_rect()
+        self.image = pygame.Surface((1, 1))
+        self.images_down = []
+        self.images_up = []
+        self.images_left = []
+        self.images_right = []
+        self.cut_sheet(Player.sheet, 9, 4)
         self.rect.x = self.x
         self.rect.y = self.y
         self.move_counter = 0
@@ -77,12 +73,19 @@ class Player(pygame.sprite.Sprite):
         self.move_right = False
         self.move_up = False
         self.move_down = True
+        self.change = [0, 0]
 
     def get_event_keyboard(self, array):
         if array[pygame.K_w] == array[pygame.K_s]:
             if array[pygame.K_a] == 1 and array[pygame.K_d] == 0:
-                self.rect.x -= 1
-                if self.move_left:
+                self.change[0] -= 1
+                if self.move_left and self.move_up:
+                    self.move_up = False
+                    self.move_counter = 0
+                elif self.move_left and self.move_down:
+                    self.move_down = False
+                    self.move_counter = 0
+                elif self.move_left:
                     self.move_counter += 1
                 else:
                     self.move_counter = 0
@@ -91,8 +94,14 @@ class Player(pygame.sprite.Sprite):
                     self.move_up = False
                     self.move_down = False
             elif array[pygame.K_a] == 0 and array[pygame.K_d] == 1:
-                self.rect.x += 1
-                if self.move_right:
+                self.change[0] += 1
+                if self.move_right and self.move_up:
+                    self.move_up = False
+                    self.move_counter = 0
+                elif self.move_right and self.move_down:
+                    self.move_down = False
+                    self.move_counter = 0
+                elif self.move_right:
                     self.move_counter += 1
                 else:
                     self.move_counter = 0
@@ -104,7 +113,7 @@ class Player(pygame.sprite.Sprite):
                 self.move_counter = 0
         elif array[pygame.K_a] == array[pygame.K_d]:
             if array[pygame.K_w] == 1 and array[pygame.K_s] == 0:
-                self.rect.y -= 1
+                self.change[1] -= 1
                 if self.move_up:
                     self.move_counter += 1
                 else:
@@ -114,7 +123,7 @@ class Player(pygame.sprite.Sprite):
                     self.move_up = True
                     self.move_down = False
             elif array[pygame.K_w] == 0 and array[pygame.K_s] == 1:
-                self.rect.y += 1
+                self.change[1] += 1
                 if self.move_down:
                     self.move_counter += 1
                 else:
@@ -127,68 +136,126 @@ class Player(pygame.sprite.Sprite):
                 self.move_counter = 0
         else:
             if array[pygame.K_s] == array[pygame.K_a] == 1:
-                self.rect.y += 1
-                self.rect.x -= 1
+                self.change[1] += 1
+                self.change[0] -= 1
                 if self.move_down:
                     self.move_counter += 1
                 else:
                     self.move_counter = 0
-                    self.move_left = False
+                    self.move_left = True
                     self.move_right = False
                     self.move_up = False
                     self.move_down = True
             elif array[pygame.K_s] == array[pygame.K_d] == 1:
-                self.rect.y += 1
-                self.rect.x += 1
+                self.change[1] += 1
+                self.change[0] += 1
                 if self.move_down:
                     self.move_counter += 1
                 else:
                     self.move_counter = 0
                     self.move_left = False
-                    self.move_right = False
+                    self.move_right = True
                     self.move_up = False
                     self.move_down = True
             elif array[pygame.K_w] == array[pygame.K_a] == 1:
-                self.rect.y -= 1
-                self.rect.x -= 1
+                self.change[1] -= 1
+                self.change[0] -= 1
                 if self.move_up:
                     self.move_counter += 1
                 else:
                     self.move_counter = 0
-                    self.move_left = False
+                    self.move_left = True
                     self.move_right = False
                     self.move_up = True
                     self.move_down = False
             elif array[pygame.K_w] == array[pygame.K_d] == 1:
-                self.rect.y -= 1
-                self.rect.x += 1
+                self.change[1] -= 1
+                self.change[0] += 1
                 if self.move_up:
                     self.move_counter += 1
                 else:
                     self.move_counter = 0
                     self.move_left = False
-                    self.move_right = False
+                    self.move_right = True
                     self.move_up = True
                     self.move_down = False
 
     def update(self):
-        self.move_counter = self.move_counter % 8
+        movement_phase = (self.move_counter // 4) % 8
+        movement_phase_slow = (self.move_counter // 8) % 8
+        dx = self.change[0] * 5
+        dy = self.change[1] * 5
+        self.rect.x += dx
+        self.rect.y += dy
         if self.move_up:
-            self.image = Player.images_up[self.move_counter]
+            if pygame.sprite.spritecollideany(self, horizontal_borders):
+                self.rect.y -= dy
+                self.image = self.images_up[movement_phase_slow]
+            else:
+                self.image = self.images_up[movement_phase]
+            if self.move_right or self.move_left:
+                if pygame.sprite.spritecollideany(self, vertical_borders):
+                    self.rect.x -= dx
+
         elif self.move_down:
-            self.image = Player.images_down[self.move_counter]
+            if pygame.sprite.spritecollideany(self, horizontal_borders):
+                self.rect.y -= dy
+                self.image = self.images_down[movement_phase_slow]
+            else:
+                self.image = self.images_down[movement_phase]
+            if self.move_right or self.move_left:
+                if pygame.sprite.spritecollideany(self, vertical_borders):
+                    self.rect.x -= dx
+
         elif self.move_right:
-            self.image = Player.images_right[self.move_counter]
+            if pygame.sprite.spritecollideany(self, vertical_borders):
+                self.rect.x -= dx
+                self.image = self.images_right[movement_phase_slow]
+            else:
+                self.image = self.images_right[movement_phase]
+            if pygame.sprite.spritecollideany(self, horizontal_borders):
+                self.rect.y -= dy
         elif self.move_left:
-            self.image = Player.images_left[self.move_counter]
+            if pygame.sprite.spritecollideany(self, vertical_borders):
+                self.rect.x -= dx
+                self.image = self.images_left[movement_phase_slow]
+            else:
+                self.image = self.images_left[movement_phase]
+            if pygame.sprite.spritecollideany(self, horizontal_borders):
+                self.rect.y -= dy
+
+        self.change = [0, 0]
+
+    def cut_sheet(self, sheet, columns, rows):
+        w = sheet.get_width() // columns
+        h = sheet.get_height() // rows
+        self.rect = pygame.Rect(0, 0, 32, 48)
+        for j in range(rows):
+            temp = []
+            for i in range(columns):
+                a = sheet.subsurface(pygame.Rect((w * i +
+                                                  16, h * j + 14), self.rect.size))
+                a = pygame.transform.scale(a, (80, 45))
+                temp.append(a)
+            if j == 0:
+                self.images_up = temp
+            elif j == 1:
+                self.images_left = temp
+            elif j == 2:
+                self.images_down = temp
+            elif j == 3:
+                self.images_right = temp
+
+    def shoot(self):
+        pass
 
 
-usuf = Player((20, 20))
+usuf = Player((100, 100))
 
 
-class mob(pygame.sprite.Sprite):
+class Mob(pygame.sprite.Sprite):
     def __init__(self, room, activate, x, y):
-        super().__init__(all_sprites)
+        super().__init__(mob_group, all_sprites)
         self.coordinates_of_mob = []
         self.activate = activate
         self.room = room
@@ -196,7 +263,7 @@ class mob(pygame.sprite.Sprite):
         self.y = random.randint(0, 560)
 
 
-class Begaet(mob):
+class Begaet(Mob):
     image = load_image("Player.png", -1)
 
     def __init__(self, room, activate, x, y):
@@ -219,7 +286,7 @@ class Begaet(mob):
             self.rect.y -= 1
 
 
-class strelyaet(mob):
+class Strelyaet(Mob):
     def __init__(self, room, activate, x, y):
         super().__init__(room, activate, x, y)
         sprite = pygame.sprite.Sprite()
@@ -229,6 +296,11 @@ class strelyaet(mob):
         sprite.rect.y = self.y + room[1] * const
 
 
+Border(50, 30, width - 98, 30)
+Border(50, height - 60, width - 98, height - 60)
+Border(50, 30, 50, height - 60)
+Border(width - 98, 30, width - 98, height - 60)
+
 clock = pygame.time.Clock()
 running = True
 while running:
@@ -236,9 +308,11 @@ while running:
         if event.type == pygame.QUIT:
             running = False
     usuf.get_event_keyboard(pygame.key.get_pressed())
-    screen.fill(pygame.Color("white"))
+    screen.blit(room_image, (0, 0))
     all_sprites.draw(screen)
+    for x in mob_group:
+        x.move()
     all_sprites.update()
     pygame.display.flip()
-    clock.tick(200)
+    clock.tick(60)
 pygame.quit()
--- usufdemo.py
+++ usufdemo.py
@@ -1,15 +1,24 @@
 import pygame
 import random
 import os
-import math
 
 FPS = 60
+
 pygame.init()
-size = width, height = 800, 600
+size = width, height = 960, 540
 screen = pygame.display.set_mode(size)
 const = 3
 mobs = pygame.sprite.Group()
+
 all_sprites = pygame.sprite.Group()
+mob_group = pygame.sprite.Group()
+player_group = pygame.sprite.Group()
+player_bullets = pygame.sprite.Group()
+vertical_borders = pygame.sprite.Group()
+horizontal_borders = pygame.sprite.Group()
+vertical_hole_borders = pygame.sprite.Group()
+horizontal_hole_borders = pygame.sprite.Group()
+object_borders = pygame.sprite.Group()
 
 
 def load_image(name, colorkey=None):
@@ -23,53 +32,43 @@ def load_image(name, colorkey=None):
     image.convert_alpha()
     if colorkey is not None:
         if colorkey is -1:
-            colorkey = image.get_at((0, 0))
+            colorkey = image.get_at((3, 3))
+        image.set_colorkey(colorkey)
 
     return image
 
 
-class Board:
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
-        self.board = [[0] * width for _ in range(height)]
-        self.left = 10
-        self.top = 10
-        self.cell_size = 30
+room_image = load_image("room1.bmp")
+room_image = pygame.transform.scale(room_image, (960, 540))
 
-    def set_view(self, left, top, cell_size):
-        self.left = left
-        self.top = top
-        self.cell_size = cell_size
 
-    def render(self):
-
-        for i in range(self.height):
-            for q in range(self.width):
-                x = (self.cell_size, self.cell_size)
-                start = (self.left + self.cell_size * q, self.top + self.cell_size * i)
-                pygame.draw.rect(screen, pygame.Color(255, 255, 255), start + x, 1)
+class Border(pygame.sprite.Sprite):
+    # строго вертикальный или строго горизонтальный отрезок
+    def __init__(self, x1, y1, x2, y2):
+        super().__init__(all_sprites)
+        if x1 == x2:  # вертикальная стенка
+            self.add(vertical_borders)
+            self.image = pygame.Surface([1, y2 - y1])
+            self.rect = pygame.Rect(x1, y1, 1, y2 - y1)
+        else:  # горизонтальная стенка
+            self.add(horizontal_borders)
+            self.image = pygame.Surface([x2 - x1, 1])
+            self.rect = pygame.Rect(x1, y1, x2 - x1, 1)
 
 
 class Player(pygame.sprite.Sprite):
-    image = load_image("Pudge.png")
-    images_down = []
-    images_up = []
-    images_left = []
-    images_right = []
-    for i in range(8):
-        images_up.append(load_image("back_%d.png" % i))
-        images_down.append(load_image("back_%d.png" % i))
-        images_left.append(load_image("back_%d.png" % i))
-        images_right.append(load_image("back_%d.png" % i))
+    sheet = load_image("playersanim.bmp", -1)
 
     def __init__(self, cords):
-        super().__init__(all_sprites)
-        self.image = Player.image
+        super().__init__(player_group, all_sprites)
         self.x = cords[0]
         self.y = cords[1]
-        self.image = pygame.transform.scale(self.image, (50, 50))
-        self.rect = self.image.get_rect()
+        self.image = pygame.Surface((1, 1))
+        self.images_down = []
+        self.images_up = []
+        self.images_left = []
+        self.images_right = []
+        self.cut_sheet(Player.sheet, 9, 4)
         self.rect.x = self.x
         self.rect.y = self.y
         self.move_counter = 0
@@ -77,12 +76,12 @@ class Player(pygame.sprite.Sprite):
         self.move_right = False
         self.move_up = False
         self.move_down = True
-
+        self.change = [0, 0]
 
     def get_event_keyboard(self, array):
         if array[pygame.K_w] == array[pygame.K_s]:
             if array[pygame.K_a] == 1 and array[pygame.K_d] == 0:
-                self.rect.x -= 1
+                self.change[0] -= 1
                 if self.move_left:
                     self.move_counter += 1
                 else:
@@ -92,7 +91,7 @@ class Player(pygame.sprite.Sprite):
                     self.move_up = False
                     self.move_down = False
             elif array[pygame.K_a] == 0 and array[pygame.K_d] == 1:
-                self.rect.x += 1
+                self.change[0] += 1
                 if self.move_right:
                     self.move_counter += 1
                 else:
@@ -105,7 +104,7 @@ class Player(pygame.sprite.Sprite):
                 self.move_counter = 0
         elif array[pygame.K_a] == array[pygame.K_d]:
             if array[pygame.K_w] == 1 and array[pygame.K_s] == 0:
-                self.rect.y -= 1
+                self.change[1] -= 1
                 if self.move_up:
                     self.move_counter += 1
                 else:
@@ -115,7 +114,7 @@ class Player(pygame.sprite.Sprite):
                     self.move_up = True
                     self.move_down = False
             elif array[pygame.K_w] == 0 and array[pygame.K_s] == 1:
-                self.rect.y += 1
+                self.change[1] += 1
                 if self.move_down:
                     self.move_counter += 1
                 else:
@@ -128,8 +127,8 @@ class Player(pygame.sprite.Sprite):
                 self.move_counter = 0
         else:
             if array[pygame.K_s] == array[pygame.K_a] == 1:
-                self.rect.y += 1
-                self.rect.x -= 1
+                self.change[1] += 1
+                self.change[0] -= 1
                 if self.move_down:
                     self.move_counter += 1
                 else:
@@ -139,8 +138,8 @@ class Player(pygame.sprite.Sprite):
                     self.move_up = False
                     self.move_down = True
             elif array[pygame.K_s] == array[pygame.K_d] == 1:
-                self.rect.y += 1
-                self.rect.x += 1
+                self.change[1] += 1
+                self.change[0] += 1
                 if self.move_down:
                     self.move_counter += 1
                 else:
@@ -150,8 +149,8 @@ class Player(pygame.sprite.Sprite):
                     self.move_up = False
                     self.move_down = True
             elif array[pygame.K_w] == array[pygame.K_a] == 1:
-                self.rect.y -= 1
-                self.rect.x -= 1
+                self.change[1] -= 1
+                self.change[0] -= 1
                 if self.move_up:
                     self.move_counter += 1
                 else:
@@ -161,8 +160,8 @@ class Player(pygame.sprite.Sprite):
                     self.move_up = True
                     self.move_down = False
             elif array[pygame.K_w] == array[pygame.K_d] == 1:
-                self.rect.y -= 1
-                self.rect.x += 1
+                self.change[1] -= 1
+                self.change[0] += 1
                 if self.move_up:
                     self.move_counter += 1
                 else:
@@ -173,90 +172,65 @@ class Player(pygame.sprite.Sprite):
                     self.move_down = False
 
     def update(self):
-        self.move_counter = self.move_counter % 8
+        movement_phase = (self.move_counter // 2) % 8
         if self.move_up:
-            self.image = Player.images_up[self.move_counter]
+            self.image = self.images_up[movement_phase]
         elif self.move_down:
-            self.image = Player.images_down[self.move_counter]
+            self.image = self.images_down[movement_phase]
         elif self.move_right:
-            self.image = Player.images_right[self.move_counter]
+            self.image = self.images_right[movement_phase]
         elif self.move_left:
-            self.image = Player.images_left[self.move_counter]
+            self.image = self.images_left[movement_phase]
+        self.rect.x += self.change[0] * 3
+        self.rect.y += self.change[1] * 3
+        self.change = [0, 0]
+
+    def cut_sheet(self, sheet, columns, rows):
+        w = sheet.get_width() // columns
+        h = sheet.get_height() // rows
+        self.rect = pygame.Rect(0, 0, 32, 48)
+        for j in range(rows):
+            temp = []
+            for i in range(columns):
+                a = sheet.subsurface(pygame.Rect((w * i +
+                                                  16, h * j + 14), self.rect.size))
+                a = pygame.transform.scale(a, (80, 45))
+                temp.append(a)
+            if j == 0:
+                self.images_up = temp
+            elif j == 1:
+                self.images_left = temp
+            elif j == 2:
+                self.images_down = temp
+            elif j == 3:
+                self.images_right = temp
+
+    def shoot(self):
+        pass
 
 
-usuf = Player((20, 20))
+usuf = Player((100, 100))
 
 
-class mob(pygame.sprite.Sprite):
-    def __init__(self):
-        self.list=[]
-        self.patrons_list=[]
-        self.sprites=pygame.sprite.Group()
-
-    def update_person(self,number):
-        if(self.list[number][2]=='migel_strelok'):
-            start_coords=self.list[number][0][0],self.list[number][0][1]
-            finish_coords=usuf.rect.x,usuf.rect.y
+class Mob(pygame.sprite.Sprite):
+    def __init__(self, room, activate, x, y):
+        super().__init__(mob_group, all_sprites)
+        self.coordinates_of_mob = []
+        self.activate = activate
+        self.room = room
+        self.x = random.randint(0, 560)
+        self.y = random.randint(0, 560)
 
 
-            speed=(finish_coords[0]-start_coords[0])/10,(finish_coords[1]-start_coords[1])/10
+class Begaet(Mob):
+    image = load_image("Player.png", -1)
 
-
-            mobs.patrons_list.append([start_coords,speed,'patron'])
-    def update_patrons(self):
-        i=0
-        global height,width
-        while(i<(len(self.patrons_list))):
-
-            if(self.patrons_list[i][-1]=='patron'):
-
-                if(self.patrons_list[i][0][0]>width or self.patrons_list[i][0][0]<0 or self.patrons_list[i][0][1]>height or self.patrons_list[i][0][1]<0):
-                    del self.patrons_list[i]
-                    i-=1
-                else:
-                    k=self.patrons_list[i][0]
-                    s=self.patrons_list[i][1]
-                    self.patrons_list[i][0]=(k[0]+s[0]*0.5,k[1]+s[1]*0.5)
-
-            i+=1
-
-
-
-    def render(self):
-        self.sprites=pygame.sprite.Group()
-        for i in range(len(self.list)):
-            if(self.list[i][-1]=='migel_strelok'):
-                sprite=pygame.sprite.Sprite()
-                sprite.image=pygame.transform.scale(load_image('migel.png'),(50,50))
-                sprite.rect=sprite.image.get_rect()
-                sprite.rect.x=self.list[i][0][0]
-                sprite.rect.y=self.list[i][0][1]
-                self.sprites.add(sprite)
-        for i in range(len(self.patrons_list)):
-            if(self.patrons_list[i][-1]=='patron'):
-                sprite=pygame.sprite.Sprite()
-                sprite.image=pygame.transform.scale(load_image('pula_migela.png'),(40,20))
-                sprite.rect=sprite.image.get_rect()
-                sprite.rect.x=self.patrons_list[i][0][0]
-                sprite.rect.y=self.patrons_list[i][0][1]
-                self.sprites.add(sprite)
-
-
-
-
-
-
-mobs=mob()
-
-class Begaet():
-    image = load_image("migel.png", -1)
-    def __init__(self, room, x, y):
+    def __init__(self, room, activate, x, y):
+        super().__init__(room, activate, x, y)
         self.image = pygame.transform.scale(Begaet.image, (50, 50))
         self.rect = self.image.get_rect()
-        self.rect.x = self.x + room[0] * self.const
-        self.rect.y = self.y + room[1] * self.const
-        mobs.list.append([x,y,random.randint(10,20)])
-
+        self.rect.x = self.x + room[0] * const
+        self.rect.y = self.y + room[1] * const
 
     def move(self):
         a = usuf.rect.x
@@ -271,58 +245,33 @@ class Begaet():
             self.rect.y -= 1
 
 
-class strelyaet():
-    def __init__(self, room,  x=0, y=0):
-        global const
+class Strelyaet(Mob):
+    def __init__(self, room, activate, x, y):
+        super().__init__(room, activate, x, y)
         sprite = pygame.sprite.Sprite()
-        sprite.image = load_image('migel.png',-1)
+        sprite.image = Begaet.image
         sprite.rect = sprite.image.get_rect()
-        self.x=max(random.randint(1,560),x)
-        self.y=max(random.randint(1,560),y)
         sprite.rect.x = self.x + room[0] * const
         sprite.rect.y = self.y + room[1] * const
-        mobs.list.append([(self.x,self.y),2000,'migel_strelok'])
 
 
-
-
+Border(50, 30, width - 98, 30)
+Border(50, height - 60, width - 98, height - 60)
+Border(50, 30, 50, height - 60)
+Border(width - 98, 30, width - 98, height - 60)
 
 clock = pygame.time.Clock()
 running = True
-
-strelyaet((3,4))
-strelyaet((2,5))
-strelyaet((7,19))
-print (mobs.list)
-
-all_time=0
-MYEVENTTYPE_1=30
-pygame.time.set_timer(MYEVENTTYPE_1,2000)
-MYEVENTTYPE_2=31
-pygame.time.set_timer(MYEVENTTYPE_2,500)
 while running:
-    screen.fill((255,255,255))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        if event.type == MYEVENTTYPE_1:
-            all_time+=500
-            for u in range(len(mobs.list)):
-                if(mobs.list[u][1]<=all_time):
-                    mobs.update_person(u)
-                    mobs.list[u][1]=mobs.list[u][1]+5000
-            pygame.time.set_timer(MYEVENTTYPE_1,2000)
-        if event.type == MYEVENTTYPE_2:
-             mobs.update_patrons()
-             mobs.render()
-             pygame.time.set_timer(MYEVENTTYPE_2,500)
-
     usuf.get_event_keyboard(pygame.key.get_pressed())
-
+    screen.blit(room_image, (0, 0))
     all_sprites.draw(screen)
-    mobs.sprites.draw(screen)
+    for x in mob_group:
+        x.move()
     all_sprites.update()
     pygame.display.flip()
-    clock.tick(200)
-
+    clock.tick(100)
 pygame.quit()
